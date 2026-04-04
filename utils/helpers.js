e832b07b3be25ca9e6e442ca2c2fd7cd9765d193
balbharticochingcenter <balbharticoching.center@gmail.com>
2026-04-04 14:35:51 +0530

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,3 @@
-// Get session end date (March 7 of next year)
 function getSessionEndDate(startDate) {
     const endDate = new Date(startDate);
     endDate.setFullYear(endDate.getFullYear() + 1);
@@ -8,12 +7,10 @@ function getSessionEndDate(startDate) {
     return endDate;
 }
 
-// Get session name
 function getSessionName(startDate, endDate) {
     return `${startDate.getFullYear()}-${endDate.getFullYear()}`;
 }
 
-// Calculate pro-rated fees
 function calculateProRatedFees(monthlyFees, monthDate, joinDate, sessionEndDate) {
     const monthStart = new Date(monthDate.getFullYear(), monthDate.getMonth(), 1);
     const monthEnd = new Date(monthDate.getFullYear(), monthDate.getMonth() + 1, 0);
@@ -31,11 +28,9 @@ function calculateProRatedFees(monthlyFees, monthDate, joinDate, sessionEndDate)
     return Math.round(proRatedFees * 100) / 100;
 }
 
-// Generate fees history
 function generateFeesHistory(joiningDate, monthlyFees, sessionEndDate) {
     const feesHistory = [];
     const sessionName = getSessionName(joiningDate, sessionEndDate);
-    
     let currentDate = new Date(joiningDate.getFullYear(), joiningDate.getMonth(), 1);
     
     while (currentDate <= sessionEndDate) {
@@ -58,42 +53,14 @@ function generateFeesHistory(joiningDate, monthlyFees, sessionEndDate) {
                 remarks: amount < monthlyFees ? `Pro-rated fees (joined on ${joiningDate.toLocaleDateString()})` : ''
             });
         }
-        
         currentDate.setMonth(currentDate.getMonth() + 1);
     }
-    
     return feesHistory;
-}
-
-// Check if student should be auto-blocked
-function shouldAutoBlock(student) {
-    const today = new Date();
-    const currentMonth = today.getMonth();
-    const currentYear = today.getFullYear();
-    const currentSession = student.currentSession.sessionName;
-    
-    // Get current and previous month fees
-    const currentMonthFee = student.feesHistory.find(f => 
-        f.monthIndex === currentMonth && f.year === currentYear && f.sessionName === currentSession
-    );
-    
-    const prevMonthFee = student.feesHistory.find(f => 
-        f.monthIndex === currentMonth - 1 && f.year === currentYear && f.sessionName === currentSession
-    );
-    
-    // If previous month is unpaid and current month is also unpaid -> block
-    if (prevMonthFee && prevMonthFee.status === 'unpaid' && 
-        currentMonthFee && currentMonthFee.status === 'unpaid') {
-        return { shouldBlock: true, reason: '2 months fees due' };
-    }
-    
-    return { shouldBlock: false };
 }
 
 module.exports = {
     getSessionEndDate,
     getSessionName,
     calculateProRatedFees,
-    generateFeesHistory,
-    shouldAutoBlock
+    generateFeesHistory
 };
